70948449d7db89caa7686ca1e6dc58360d877d5c
farhan rafid syauqi <farhanrafid97@gmail.com>
2024-03-15 23:30:28 +0700

    feat: migrate to typescript on api

---
+++ api/src/config/database.ts
@@ -0,0 +1,11 @@
+import mysql from "mysql";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const connection = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+});
